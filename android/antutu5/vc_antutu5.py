2c64823ba3246f5f64de89b3d13f12c0b4318729
Sungwoo Park <swpark@nexell.co.kr>
2014-10-28 15:37:51 +0900

    [FIX] get score and print debugging for antutu5

--- android/antutu5/vc_antutu5.py
+++ android/antutu5/vc_antutu5.py
@@ -51,12 +51,11 @@ while(not finished):
 	try:
 #		progress_button = vc.findViewByIdOrRaise("com.antutu.ABenchMark:id/total_score_layout")
 		progress_button = vc.findViewByIdOrRaise("com.antutu.ABenchMark:id/main_chart_layout")
-		finished = True 
+		finished = True
 	except ViewNotFoundException:
 		pass
 
 print ("Benchmark Finished")
-"""
 time.sleep(2)
 detail_button = vc.findViewByIdOrRaise("com.antutu.ABenchMark:id/detail_btn")
 detail_button.touch()
@@ -77,19 +76,34 @@ threed_graphics_score = vc.findViewByIdOrRaise("com.antutu.ABenchMark:id/gpu_3d_
 storage_io_score = vc.findViewByIdOrRaise("com.antutu.ABenchMark:id/io_sdw_text")
 database_io_score = vc.findViewByIdOrRaise("com.antutu.ABenchMark:id/io_db_text")
 
-call(['lava-test-case', '"AnTuTu 5.0 Alpha UX Multitask Score"', '--result', 'pass', '--measurement', multitask_score.getText()])
-call(['lava-test-case', '"AnTuTu 5.0 Alpha UX Runtime Score"', '--result', 'pass', '--measurement', runtime_score.getText()])
-call(['lava-test-case', '"AnTuTu 5.0 Alpha RAM Operation Score"', '--result', 'pass', '--measurement', ram_operation_score.getText()])
-call(['lava-test-case', '"AnTuTu 5.0 Alpha RAM Speed Score"', '--result', 'pass', '--measurement', ram_speed_score.getText()])
-call(['lava-test-case', '"AnTuTu 5.0 Alpha Multi Core CPU Integer Score"', '--result', 'pass', '--measurement', cpu_multi_integer_score.getText()])
-call(['lava-test-case', '"AnTuTu 5.0 Alpha Multi Core CPU Float-Point Score"', '--result', 'pass', '--measurement', cpu_multi_float_point_score.getText()])
-call(['lava-test-case', '"AnTuTu 5.0 Alpha Single Thread CPU Integer Score"', '--result', 'pass', '--measurement', cpu_single_integer_score.getText()])
-call(['lava-test-case', '"AnTuTu 5.0 Alpha Single Thread CPU Float-Point Score"', '--result', 'pass', '--measurement', cpu_single_float_point_score.getText()])
-call(['lava-test-case', '"AnTuTu 5.0 Alpha GPU 2D Graphics Score"', '--result', 'pass', '--measurement', twod_graphics_score.getText()])
-call(['lava-test-case', '"AnTuTu 5.0 Alpha GPU 3D Graphics Score"', '--result', 'pass', '--measurement', threed_graphics_score.getText()])
-call(['lava-test-case', '"AnTuTu 5.0 Alpha IO Storage I/O Score"', '--result', 'pass', '--measurement', storage_io_score.getText()])
-call(['lava-test-case', '"AnTuTu 5.0 Alpha IO Database I/O Score"', '--result', 'pass', '--measurement', database_io_score.getText()])
-"""
+# psw0523 debugging
+print(['lava-test-case', '"AnTuTu 5.0 Alpha UX Multitask Score"', '--result', 'pass', '--measurement', multitask_score.getText()])
+print(['lava-test-case', '"AnTuTu 5.0 Alpha UX Runtime Score"', '--result', 'pass', '--measurement', runtime_score.getText()])
+print(['lava-test-case', '"AnTuTu 5.0 Alpha RAM Operation Score"', '--result', 'pass', '--measurement', ram_operation_score.getText()])
+print(['lava-test-case', '"AnTuTu 5.0 Alpha RAM Speed Score"', '--result', 'pass', '--measurement', ram_speed_score.getText()])
+print(['lava-test-case', '"AnTuTu 5.0 Alpha Multi Core CPU Integer Score"', '--result', 'pass', '--measurement', cpu_multi_integer_score.getText()])
+print(['lava-test-case', '"AnTuTu 5.0 Alpha Multi Core CPU Float-Point Score"', '--result', 'pass', '--measurement', cpu_multi_float_point_score.getText()])
+print(['lava-test-case', '"AnTuTu 5.0 Alpha Single Thread CPU Integer Score"', '--result', 'pass', '--measurement', cpu_single_integer_score.getText()])
+print(['lava-test-case', '"AnTuTu 5.0 Alpha Single Thread CPU Float-Point Score"', '--result', 'pass', '--measurement', cpu_single_float_point_score.getText()])
+print(['lava-test-case', '"AnTuTu 5.0 Alpha GPU 2D Graphics Score"', '--result', 'pass', '--measurement', twod_graphics_score.getText()])
+print(['lava-test-case', '"AnTuTu 5.0 Alpha GPU 3D Graphics Score"', '--result', 'pass', '--measurement', threed_graphics_score.getText()])
+print(['lava-test-case', '"AnTuTu 5.0 Alpha IO Storage I/O Score"', '--result', 'pass', '--measurement', storage_io_score.getText()])
+print(['lava-test-case', '"AnTuTu 5.0 Alpha IO Database I/O Score"', '--result', 'pass', '--measurement', database_io_score.getText()])
+
+# this is org
+#call(['lava-test-case', '"AnTuTu 5.0 Alpha UX Multitask Score"', '--result', 'pass', '--measurement', multitask_score.getText()])
+#call(['lava-test-case', '"AnTuTu 5.0 Alpha UX Runtime Score"', '--result', 'pass', '--measurement', runtime_score.getText()])
+#call(['lava-test-case', '"AnTuTu 5.0 Alpha RAM Operation Score"', '--result', 'pass', '--measurement', ram_operation_score.getText()])
+#call(['lava-test-case', '"AnTuTu 5.0 Alpha RAM Speed Score"', '--result', 'pass', '--measurement', ram_speed_score.getText()])
+#call(['lava-test-case', '"AnTuTu 5.0 Alpha Multi Core CPU Integer Score"', '--result', 'pass', '--measurement', cpu_multi_integer_score.getText()])
+#call(['lava-test-case', '"AnTuTu 5.0 Alpha Multi Core CPU Float-Point Score"', '--result', 'pass', '--measurement', cpu_multi_float_point_score.getText()])
+#call(['lava-test-case', '"AnTuTu 5.0 Alpha Single Thread CPU Integer Score"', '--result', 'pass', '--measurement', cpu_single_integer_score.getText()])
+#call(['lava-test-case', '"AnTuTu 5.0 Alpha Single Thread CPU Float-Point Score"', '--result', 'pass', '--measurement', cpu_single_float_point_score.getText()])
+#call(['lava-test-case', '"AnTuTu 5.0 Alpha GPU 2D Graphics Score"', '--result', 'pass', '--measurement', twod_graphics_score.getText()])
+#call(['lava-test-case', '"AnTuTu 5.0 Alpha GPU 3D Graphics Score"', '--result', 'pass', '--measurement', threed_graphics_score.getText()])
+#call(['lava-test-case', '"AnTuTu 5.0 Alpha IO Storage I/O Score"', '--result', 'pass', '--measurement', storage_io_score.getText()])
+#call(['lava-test-case', '"AnTuTu 5.0 Alpha IO Database I/O Score"', '--result', 'pass', '--measurement', database_io_score.getText()])
+
 '''
 time.sleep(3)
 menu_back_icon = vc.findViewByIdOrRaise("com.antutu.ABenchMark:id/menu_back_img")
